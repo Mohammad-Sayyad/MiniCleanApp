b7bb366bda4bd31f981acb53374ae4eedb61f2dd
mo.sayad <sayyadamir71111@gmail.com>
2026-08-11 09:16:22 +0330

    .

---
+++ src/AzmoonGaj.Domain/Entities/UserAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzmoonGaj.Domain.Entities
+{
+    public class UserAccount
+    {
+        public long Id { get; set; }
+
+        public long UserId { get; set; }
+
+        public string Username { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+
+        public string? Email { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime? LastLoginAt { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+
+        // Navigation Property
+        public Users User { get; set; } = null!;
+    }
+}
